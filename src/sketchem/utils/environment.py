bb8689a1b3fc8e7c4c6896c4c848116405f8feb0
axelbrunetta <axel.brunetta@gmail.com>
2025-04-27 18:58:11 +0200

    Previous commit worked!

--- src/sketchem/utils/environment.py
+++ src/sketchem/utils/environment.py
@@ -4,5 +4,5 @@ import platform
 # Behavior for streamlit is different in cloud and locally run -> especially for what we will do with the databse, hence the need for this
 
 def is_running_locally():
-    """Check if the app is running locally or in Streamlit cloud"""
+    """Check if the app is running locally or in Streamlit cloud -> this works since Streamlit Cloud runs on Linux without a processor name"""
     return platform.processor() != ''
