d88a4b7e4776d3ba04f25be046c156d301e5a1c8
vertippz <ivana.josipovic@epfl.ch>
2025-04-29 23:23:09 +0200

    adding categories

---
+++ data/molecule_data.py
@@ -0,0 +1,63 @@
+
+MOLECULE_CATEGORIES = {
+    "Amino Acids": [
+        "Alanine", "Arginine", "Asparagine", "Aspartic acid", "Cysteine",
+        "Glutamine", "Glutamic acid", "Glycine", "Histidine", "Isoleucine",
+        "Leucine", "Lysine", "Methionine", "Phenylalanine", "Proline",
+        "Serine", "Threonine", "Tryptophan", "Tyrosine", "Valine"
+    ],
+    "Organic Acids": [
+        "Acetic acid", "Citric acid", "Lactic acid", "Formic acid",
+        "Oxalic acid", "Malic acid", "Succinic acid", "Tartaric acid"
+    ],
+    "Alkenes and Alcohols": [
+        "Methanol", "Ethanol", "Propanol", "Butanol", "Isopropanol", 
+        "Glycerol", "Ethene", "Propene", "Butene", "Pentene"
+    ]
+}
+
+MOLECULE_SMILES = {
+    # Amino acids
+    "Alanine": "CC(C(=O)O)N",
+    "Arginine": "C(CCCN=C(N)N)C(C(=O)O)N",
+    "Asparagine": "C(C(C(=O)O)N)C(=O)N",
+    "Aspartic acid": "C(C(C(=O)O)N)C(=O)O",
+    "Cysteine": "C(C(C(=O)O)N)S",
+    "Glutamine": "C(CC(=O)N)C(C(=O)O)N",
+    "Glutamic acid": "C(CC(=O)O)C(C(=O)O)N",
+    "Glycine": "C(C(=O)O)N",
+    "Histidine": "C(C(C(=O)O)N)C1=CN=CN1",
+    "Isoleucine": "CC(C)CC(C(=O)O)N",
+    "Leucine": "CC(C)C(C(=O)O)N",
+    "Lysine": "C(CCCN)C(C(=O)O)N",
+    "Methionine": "CSCC(C(=O)O)N",
+    "Phenylalanine": "C(C(C(=O)O)N)C1=CC=CC=C1",
+    "Proline": "C1CC(NC1)C(=O)O",
+    "Serine": "C(C(C(=O)O)N)O",
+    "Threonine": "C[C@H](O)[C@@H](C(=O)O)N",
+    "Tryptophan": "C(C(C(=O)O)N)C1=CNC2=CC=CC=C12",
+    "Tyrosine": "C(C(C(=O)O)N)C1=CC=C(C=C1)O",
+    "Valine": "CC(C)C(C(=O)O)N",
+
+    # Organic acids
+    "Acetic acid": "CC(=O)O",
+    "Citric acid": "C(C(=O)O)C(CC(=O)O)(C(=O)O)O",
+    "Lactic acid": "CC(C(=O)O)O",
+    "Formic acid": "C(=O)O",
+    "Oxalic acid": "OC(=O)C(=O)O",
+    "Malic acid": "C(C(C(=O)O)O)(=O)O",
+    "Succinic acid": "OC(=O)CCC(=O)O",
+    "Tartaric acid": "OC(=O)C(C(C(=O)O)O)O",
+
+    # Alkenes and Alcohols
+    "Methanol": "CO",
+    "Ethanol": "CCO",
+    "Propanol": "CCCO",
+    "Butanol": "CCCCO",
+    "Isopropanol": "CC(C)O",
+    "Glycerol": "C(C(CO)O)O",
+    "Ethene": "C=C",
+    "Propene": "C=CC",
+    "Butene": "C=CCC",
+    "Pentene": "CCC=CC"
+}
