c7cf99cfca79dc765d503f3766222d840485911a
vertippz <ivana.josipovic@epfl.ch>
2025-05-15 01:24:31 +0200

    fix single plqyer setup

--- src/sketchem/main.py
+++ src/sketchem/main.py
@@ -6,9 +6,13 @@ from sketchem.pages.waiting_room import render_waiting_room
 from sketchem.utils.toast import display_queued_toast
 from sketchem.pages.game_page_single import render_game_page
 from sketchem.pages.game_page_multi import render_game_page_multi
+from sketchem.pages.single_player_setup import render_singleplayer_setup
+
 
 def main():
-    st.set_page_config(page_title="Sketchem", layout="centered", initial_sidebar_state="collapsed")
+    st.set_page_config(
+        page_title="Sketchem", layout="centered", initial_sidebar_state="collapsed"
+    )
 
     st.markdown(
         """
@@ -26,7 +30,7 @@ def main():
     """,
         unsafe_allow_html=True,
     )
-    display_queued_toast() #Show any active toast notifications
+    display_queued_toast()  # Show any active toast notifications
 
     st.title("🧪 Sketchem")
 
@@ -36,17 +40,22 @@ def main():
     # Route to appropriate page based on game mode chosen
     if st.session_state.game_mode is None:
         render_home_page()
-    elif st.session_state.game_mode == "singleplayer_setup":
-        # Render setup for single here
-        pass
-    elif st.session_state.game_mode == "multiplayer_setup": # reroute to multiplayer setup page
+    elif st.session_state.game_mode == "single_setup":  # Changed to match home page
+        render_singleplayer_setup()
+    elif (
+        st.session_state.game_mode == "multiplayer_setup"
+    ):  # reroute to multiplayer setup page
         render_multiplayer_setup()
-    elif st.session_state.game_mode in ["created_multi", "joined_multi"]: # reroute to waiting room for both host and joining players
+    elif st.session_state.game_mode in [
+        "created_multi",
+        "joined_multi",
+    ]:  # reroute to waiting room for both host and joining players
         render_waiting_room()
     elif st.session_state.game_mode == "single":
         render_game_page()
     elif st.session_state.game_mode == "multiplayer":
         render_game_page_multi()
+
 
 if __name__ == "__main__":
     main()
