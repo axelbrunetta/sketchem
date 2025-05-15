516da1046b8d9d0fd804288567656bfedbadb07f
axelbrunetta <axel.brunetta@gmail.com>
2025-05-15 22:18:39 +0200

    Fixed local api key

--- src/sketchem/utils/environment.py
+++ src/sketchem/utils/environment.py
@@ -3,6 +3,7 @@ import platform
 import os
 import streamlit as st
 
+
 # Behavior for streamlit is different in cloud and locally run -> especially for what we will do with the databse, hence the need for this
 
 def is_running_locally():
@@ -15,4 +16,6 @@ def get_gemini_api_key():
         if "GEMINI_API_KEY" in st.secrets:
             return st.secrets["GEMINI_API_KEY"]
     except Exception:
+        from dotenv import load_dotenv
+        load_dotenv()  # Load environment variables from .env file
         return os.environ.get("GEMINI_API_KEY")
