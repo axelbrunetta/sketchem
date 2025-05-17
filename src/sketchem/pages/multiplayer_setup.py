8b5d186edcfc60925155758a644757874a17e99f
axelbrunetta <axel.brunetta@gmail.com>
2025-05-17 22:26:47 +0200

    Added WORKING screen width detection (does not update when window is resized though) and added columns to home and multiplayer setup pages to fix issues caused by this change

--- src/sketchem/pages/multiplayer_setup.py
+++ src/sketchem/pages/multiplayer_setup.py
@@ -60,147 +60,151 @@ def render_multiplayer_setup():
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
     back_button(destination=None, label="Back to Home") #Display back button at the top left
-
-    st.markdown("## Multiplayer Setup")
     
-    # Player name input
-    player_name = st.text_input("Enter your name:", key="player_name_input")
-    st.session_state.player_name = player_name 
-
-    if not player_name:
-        st.warning("Please enter your name to continue")
-        return
-
+    col1, col2, col3 = st.columns([1, 2, 1])
     
-    st.divider()
 
-    col1, col2 = st.columns(2)
-   
-   
-    if 'category_update_counter' not in st.session_state:
-        st.session_state.category_update_counter = 0
+    with col2:
+        st.markdown("## Multiplayer Setup")
+        
+        # Player name input
+        player_name = st.text_input("Enter your name:", key="player_name_input")
+        st.session_state.player_name = player_name 
 
-    with col1:
-        # Initialize state 
-        if "show_new_game_button" not in st.session_state:
-            st.session_state.show_new_game_button = True
-            
-        def hide_new_game_button():
-            st.session_state.show_new_game_button = False
-            
-        # Show button or game options based on state 
-        if st.session_state.show_new_game_button:
-            st.button("New Game", on_click=hide_new_game_button, key="new_game_button", use_container_width=True)
-        else:
-            # Game settings 
-            st.markdown("### Game Settings")
+        if not player_name:
+            st.warning("Please enter your name to continue")
+            return
 
-            
-            @st.fragment()
-            def slider_fragment():
+        
+        st.divider()
+
+        col1, col2 = st.columns(2)
+    
+    
+        if 'category_update_counter' not in st.session_state:
+            st.session_state.category_update_counter = 0
+
+        with col1:
+            # Initialize state 
+            if "show_new_game_button" not in st.session_state:
+                st.session_state.show_new_game_button = True
                 
-                displayed_duration = st.slider(
-                    "Game Duration (seconds)",
-                    min_value=180,
-                    max_value=600,
-                    value=300,
-                    step=10
-                )
-                # Add 5 seconds to the actual game duration to account for loading time of the game page -> because of the timer
-                st.session_state.game_duration = displayed_duration + 5
-            slider_fragment()
-            st.session_state.enable_hints = st.toggle("Enable hints")
+            def hide_new_game_button():
+                st.session_state.show_new_game_button = False
+                
+            # Show button or game options based on state 
+            if st.session_state.show_new_game_button:
+                st.button("New Game", on_click=hide_new_game_button, key="new_game_button", use_container_width=True)
+            else:
+                # Game settings 
+                st.markdown("### Game Settings")
 
-            st.divider()
-
-            # Molecule category selection
-            st.markdown("### Molecule Category")
-
-
-            # Function to increment the counter when a new category is added
-            def increment_category_counter():
-                st.session_state.category_update_counter += 1
-
-            
-            #Generate a custom category using gemini
-            @st.dialog("Generate a molecule category")
-            def openModal():
-                st.write(f"What kind of molecule category are you looking for?")
-                user_input = st.text_input("")
-                if st.button("Submit"):
+                
+                @st.fragment()
+                def slider_fragment():
                     
-                    returned_var = get_molecules_for_category_pubchem(api_key = st.secrets.get("GEMINI_API_KEY", ""), user_prompt = user_input)
+                    displayed_duration = st.slider(
+                        "Game Duration (seconds)",
+                        min_value=180,
+                        max_value=600,
+                        value=300,
+                        step=10
+                    )
+                    # Add 5 seconds to the actual game duration to account for loading time of the game page -> because of the timer
+                    st.session_state.game_duration = displayed_duration + 5
+                slider_fragment()
+                st.session_state.enable_hints = st.toggle("Enable hints")
 
+                st.divider()
+
+                # Molecule category selection
+                st.markdown("### Molecule Category")
+
+
+                # Function to increment the counter when a new category is added
+                def increment_category_counter():
                     st.session_state.category_update_counter += 1
 
-                    logger.info(f"Generate category message: {returned_var}")
+                
+                #Generate a custom category using gemini
+                @st.dialog("Generate a molecule category")
+                def openModal():
+                    st.write(f"What kind of molecule category are you looking for?")
+                    user_input = st.text_input("")
+                    if st.button("Submit"):
+                        
+                        returned_var = get_molecules_for_category_pubchem(api_key = st.secrets.get("GEMINI_API_KEY", ""), user_prompt = user_input)
+
+                        st.session_state.category_update_counter += 1
+
+                        logger.info(f"Generate category message: {returned_var}")
+                        
+                        if returned_var == "Successfully created category":
+                            st.session_state.toast_queue = {"message": "Successfully created category.", "icon": "✅"}
+                        else:
+                            st.session_state.toast_queue = {"message": "Failed to create category, try to formulate your query differently.", "icon": "☹️"}
+                        st.rerun() #Closes the modal view
+
+                
+
+                
+
+                # Create a new list with all categories
+                all_categories = list(MOLECULE_CATEGORIES.keys())
+                if hasattr(st.session_state, 'additional_categories'):
+                    _ = st.session_state.get("category_update_counter", 0) #forces regeneration of all categories when counter changes
+                    all_categories.extend(st.session_state.additional_categories.keys())
+
+
+                #Select a category
+                
+                # Use the combined list for the selectbox
+                selected_category = st.selectbox( 
+                    "Select a molecule category:",
+                    options=all_categories,
+                    index=all_categories.index(st.session_state.selected_molecule_category) if  not st.session_state.category_is_default else 0,
+                    key=f"molecule_category_{st.session_state.get('category_update_counter', 0)}"
+                ) #Now automatically selects the last created category (when created using AI)
+                
+
+
+                
+                if st.button("Create a molecule category using AI", key="create_category_button", help="This is an experimental feature, some things may not work as intended."):
+                    openModal()
+
+                st.divider()
+
+                
+                # Clear the last_created_category after using it and update the selected category
+                if hasattr(st.session_state, 'last_created_category'):
                     
-                    if returned_var == "Successfully created category":
-                        st.session_state.toast_queue = {"message": "Successfully created category.", "icon": "✅"}
+                    # Clean up
+                    del st.session_state.last_created_category
+
+                
+
+                if selected_category:
+                    # Display molecules in selected category
+                    st.session_state.selected_molecule_category = selected_category
+                    check_category_is_default(selected_category)
+                    molecule_list = ""
+                    if st.session_state.category_is_default:
+                        for molecule in MOLECULE_CATEGORIES[selected_category].keys(): #display category if default
+                            molecule_list += f"- {molecule}<br>"
                     else:
-                        st.session_state.toast_queue = {"message": "Failed to create category, try to formulate your query differently.", "icon": "☹️"}
-                    st.rerun() #Closes the modal view
+                        for molecule in st.session_state.additional_categories[selected_category].keys(): #display category if ai generated
+                            molecule_list += f"- {molecule}<br>"
+                    
+                    stoggle(
+                    f"Molecules in {selected_category}:",
+                    f"{molecule_list}",
+                    )
 
-            
+                st.divider()
 
-            
-
-            # Create a new list with all categories
-            all_categories = list(MOLECULE_CATEGORIES.keys())
-            if hasattr(st.session_state, 'additional_categories'):
-                _ = st.session_state.get("category_update_counter", 0) #forces regeneration of all categories when counter changes
-                all_categories.extend(st.session_state.additional_categories.keys())
-
-
-            #Select a category
-            
-            # Use the combined list for the selectbox
-            selected_category = st.selectbox( 
-                "Select a molecule category:",
-                options=all_categories,
-                index=all_categories.index(st.session_state.selected_molecule_category) if  not st.session_state.category_is_default else 0,
-                key=f"molecule_category_{st.session_state.get('category_update_counter', 0)}"
-            ) #Now automatically selects the last created category (when created using AI)
-            
-
-
-            
-            if st.button("Create a molecule category using AI", key="create_category_button", help="This is an experimental feature, some things may not work as intended."):
-                openModal()
-
-            st.divider()
-
-            
-            # Clear the last_created_category after using it and update the selected category
-            if hasattr(st.session_state, 'last_created_category'):
-                
-                # Clean up
-                del st.session_state.last_created_category
-
-            
-
-            if selected_category:
-                # Display molecules in selected category
-                st.session_state.selected_molecule_category = selected_category
-                check_category_is_default(selected_category)
-                molecule_list = ""
-                if st.session_state.category_is_default:
-                    for molecule in MOLECULE_CATEGORIES[selected_category].keys(): #display category if default
-                        molecule_list += f"- {molecule}<br>"
-                else:
-                    for molecule in st.session_state.additional_categories[selected_category].keys(): #display category if ai generated
-                        molecule_list += f"- {molecule}<br>"
-                
-                stoggle(
-                f"Molecules in {selected_category}:",
-                f"{molecule_list}",
-                )
-
-            st.divider()
-
-            create_disabled = selected_category is None #Disable button below if no category selected
-            if st.button("Create New Game", key="create_new_game_button", use_container_width=True, disabled=create_disabled):
-                handle_create_game(player_name)
+                create_disabled = selected_category is None #Disable button below if no category selected
+                if st.button("Create New Game", key="create_new_game_button", use_container_width=True, disabled=create_disabled):
+                    handle_create_game(player_name)
 
     with col2:
         st.markdown("### Join Existing Game")
