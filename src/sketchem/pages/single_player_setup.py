67ada3081fc053cb0a8f1092f09afd9e39bf706b
axelbrunetta <axel.brunetta@gmail.com>
2025-05-21 14:52:02 +0200

    Messed up previous commit, changed slider values now

--- src/sketchem/pages/single_player_setup.py
+++ src/sketchem/pages/single_player_setup.py
@@ -116,7 +116,7 @@ def render_singleplayer_setup():
 
         game_duration = st.slider(
             label="Time per molecule",  #required parameter but will be hidden
-            min_value=30,
+            min_value=180,
             max_value=600,
             value=st.session_state["game_duration"],  # Use the value directly without get()
             step=10,
