b08598e0a6d23f12482a5b8ffb9bdc8391b3f012
vertippz <ivana.josipovic@epfl.ch>
2025-05-21 17:59:26 +0200

    added comments

--- src/sketchem/pages/guide_page.py
+++ src/sketchem/pages/guide_page.py
@@ -7,30 +7,31 @@ from sketchem.utils.environment import is_running_locally
 
 
 def render_guide_page():
-    # Import CSS from file
+    #import css
     css_path = os.path.join(os.path.dirname(__file__), "style", "guide_page_styling.css") if is_running_locally() else '/mount/src/sketchem/src/sketchem/pages/style/guide_page_styling.css'
     
     with open(css_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
-    # Add back button that redirects to home
+    #add back button that leads to home
     back_button(destination=None, label="Back to Home")
     
     st.markdown("<h2 style='text-align: center;'>A guide on how to use sketchem</h2>", unsafe_allow_html=True)
     
     
 
-    # Get user's home directory and construct path to Downloads
+    #get user's home directory and construct path to downloads
     current_dir = os.path.dirname(__file__)
+    #get path to pdf file
     pdf_path = os.path.join(current_dir, "..", "..", "..", "assets", "userguide.pdf")
     
-    # Check if the PDF file exists
+    #check if pdf file exists
     if os.path.exists(pdf_path):
-        # Read and display the PDF file
+        #read and display pdf file
         with open(pdf_path, "rb") as f:
             pdf_bytes = f.read()
 
-            # Display PDF using PDF viewer with custom styling
+            #display pdf using pdf viewer 
             base64_pdf = base64.b64encode(pdf_bytes).decode("utf-8")
             pdf_display = f"""
                 <div class="pdf-container">
@@ -43,6 +44,7 @@ def render_guide_page():
                 </div>
             """
             st.markdown(pdf_display, unsafe_allow_html=True)
+    #error if pdf file not found
     else:
         st.error(
             f"User guide PDF file not found. Please make sure 'userguide.pdf' exists in the data directory at: {pdf_path}"
